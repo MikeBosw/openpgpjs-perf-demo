4430e39e2e8e72e2476c98dff51c97208d8d9f48
Michael Bosworth <michael.a.bosworth@gmail.com>
2019-03-19 19:04:14 -0700

    /static -> static

--- docs/precache-manifest.95ebcf65ebc8d104f09b04ade4f01f3e.js
+++ docs/precache-manifest.95ebcf65ebc8d104f09b04ade4f01f3e.js
@@ -1,19 +1,19 @@
 self.__precacheManifest = [
   {
     "revision": "3ffcf917a0bf7e6b86a1",
-    "url": "/static/css/main.abebf9f9.chunk.css"
+    "url": "static/css/main.abebf9f9.chunk.css"
   },
   {
     "revision": "3ffcf917a0bf7e6b86a1",
-    "url": "/static/js/main.8f4e2716.chunk.js"
+    "url": "static/js/main.8f4e2716.chunk.js"
   },
   {
     "revision": "42ac5946195a7306e2a5",
-    "url": "/static/js/runtime~main.a8a9905a.js"
+    "url": "static/js/runtime~main.a8a9905a.js"
   },
   {
     "revision": "0e039d81563958aa96a3",
-    "url": "/static/js/2.972a3955.chunk.js"
+    "url": "static/js/2.972a3955.chunk.js"
   },
   {
     "revision": "12c04cc02ba3aed0cff3484facad0983",
