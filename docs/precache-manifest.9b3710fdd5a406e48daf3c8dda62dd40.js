3eb896dc05813e007bf4a900da0a5b583bc097b9
Michael Bosworth <michael.a.bosworth@gmail.com>
2019-03-19 18:23:19 -0700

    github.io site

---
+++ docs/precache-manifest.9b3710fdd5a406e48daf3c8dda62dd40.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "a335099c5eb8fe11717e",
+    "url": "/static/css/main.abebf9f9.chunk.css"
+  },
+  {
+    "revision": "a335099c5eb8fe11717e",
+    "url": "/static/js/main.3a36753e.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "0309ac5054eb1c1cacee",
+    "url": "/static/js/2.13031512.chunk.js"
+  },
+  {
+    "revision": "1b6aa1ef49dcadb1cc9c3104fb920f7e",
+    "url": "/index.html"
+  }
+];
